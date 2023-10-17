0fc930c640297552fc4ce4462d54c7e9be01427d
shwchlorine <dyen2018@gmail.com>
2023-10-17 00:51:16 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,7 @@ const container = document.getElementById("root")
 if (!container) throw new Error('Failed to find the root element');
 const root = ReactDOM.createRoot(container)
 
-root.render(
+ReactDOM.render(
     <React.StrictMode>
         <HashRouter>
             <ColorModeScript />
